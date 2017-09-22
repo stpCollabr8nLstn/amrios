0732600a16b7ffb63ddc0b8959d26ee91975b582
amrios <amrios2817@gmail.com>
2017-09-22 16:56:23 -0400

    update site style

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # amrios
+
+To run locally:
+1. `git clone https://github.com/stpCollabr8nLstn/amrios.git` 
+2. `sass scss/style.scss style.css`
+3. open `index.html` in browser
---
+++ js/index.js
@@ -0,0 +1,16 @@
+function element(selector) {
+  return document.querySelector(selector);
+}
+
+function element_all(selector) {
+  return document.querySelectorAll(selector);
+}
+
+
+function eventListener() {
+}
+
+
+window.onload = function() {
+  eventListener();
+}
